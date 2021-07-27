ba8ba9fae8cd02edaf89098fb5205e46d5523d07
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-07-27 12:00:37 +0200

    feat: add ball to top of the window

---
+++ PingPong/CsharpPong/Ball.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPong
+{
+    class Ball : Rectangle
+    {
+        public Ball(MainWindow mainWindow, System.Windows.Shapes.Rectangle visualRectangle) : base(mainWindow, visualRectangle)
+        {
+        }
+    }
+}
